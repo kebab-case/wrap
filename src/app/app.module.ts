bc477e11b17761aaeebe587d851325dd97334453
atreus <joakim.heldmann@gmail.com>
2018-01-20 16:42:17 +0100

    base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { TabsModule } from './tabs/tabs.module';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
 import { HeaderComponent } from './header/header.component';
+import { SettingsComponent } from './settings/settings.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AddComponent,
+    SettingsComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule,
